376e4221ff9a23185759f197e4453e8dd287a51e
Newton Jain <Newton Jain>
2016-06-08 01:38:45 -0700

    First commit with all project files and VS .sln file

---
+++ app/pages/getting-started/getting-started.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  templateUrl: 'build/pages/getting-started/getting-started.html'
+})
+export class GettingStartedPage {
+  constructor() {
+
+  }
+}
